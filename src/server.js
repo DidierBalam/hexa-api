dc4abeeda6c010848afbdc31d59a728ae56fa366
Didier Balam <50980339+DidierBalam@users.noreply.github.com>
2022-07-27 13:00:27 -0500

    fix

--- src/server.js
+++ src/server.js
@@ -18,9 +18,10 @@ const PORT = 8080
 
 const app = express()
 
-app.use(express.json({ extended: false }));
 app.use('/api', router)
 
 app.listen(PORT, () => {
     console.log('Servidor corriendo en el puerto', PORT)
 })
+
+module.exports = app
