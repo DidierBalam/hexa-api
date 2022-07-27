6f7a3ba3bae4a6c3b21f398c99cd2918ef1d2ae6
Didier Balam <50980339+DidierBalam@users.noreply.github.com>
2022-07-27 13:10:14 -0500

    update

--- server-local.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-const app = require('./src/server.js');
-
-app.listen(4000, () => console.log('Local app listening on port 4000!'));
--- src/server.js
+++ src/server.js
@@ -1,16 +1,3 @@
-// const express = require("express");
-// const serverless = require("serverless-http");
-// const bodyParser = require('body-parser')
-// const router = require('./routes')
-
-// const app = express();
-// app.use(bodyParser.json());
-
-// // app.use(`/.netlify/functions/server`, router);
-
-// module.exports = app;
-// // module.exports.handler = serverless(app);
-
 const express = require('express')
 const router = require('./routes')
 
