d98b8dd3521907d07be99f7050b0e4d35d3d2e9a
Didier Balam <50980339+DidierBalam@users.noreply.github.com>
2022-07-27 13:47:32 -0500

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,18 +11,18 @@ router.route('/states').get((_, res) => {
   }
 })
 
-router.route('/mayoralties/:id').get((req, res) => {
-  const mayoralties1 = require('../database/mayoralties1.json')
-  const mayoralties2 = require('../database/mayoralties02.json')
-  try {
-    const { id } = req.params
-    const features = mayoralties1.features.concat(mayoralties2.features)
-    const filter = features.filter(data => data.properties.CVE_ENT === id)
-    res.send(filter)
-  } catch (err) {
-    console.log(err)
-    res.send(err)
-  }
-})
+// router.route('/mayoralties/:id').get((req, res) => {
+//   const mayoralties1 = require('../database/mayoralties1.json')
+//   const mayoralties2 = require('../database/mayoralties02.json')
+//   try {
+//     const { id } = req.params
+//     const features = mayoralties1.features.concat(mayoralties2.features)
+//     const filter = features.filter(data => data.properties.CVE_ENT === id)
+//     res.send(filter)
+//   } catch (err) {
+//     console.log(err)
+//     res.send(err)
+//   }
+// })
 
 module.exports = router
