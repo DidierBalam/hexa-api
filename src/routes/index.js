4c1d4a5bff5d6cd65dc4943068ba293e5f300c06
Didier Balam <50980339+DidierBalam@users.noreply.github.com>
2022-07-27 21:08:59 -0500

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,31 +1,31 @@
 const express = require('express')
 const router = express.Router()
 
-router.route(`/states/:page`).get((req, res) => {
-  try {
-    // const { page } = req.params
+// router.route(`/states/:page`).get((req, res) => {
+//   try {
+//     // const { page } = req.params
 
-    // const states = require('../database/states.json')
-    // const count = parseInt(page)
+//     // const states = require('../database/states.json')
+//     // const count = parseInt(page)
 
-    // const init = 3 * (page - 1)
-    // const last = 3 * count
+//     // const init = 3 * (page - 1)
+//     // const last = 3 * count
 
-    // console.log('position', init, last)
+//     // console.log('position', init, last)
 
-    // res.send(states.features.slice(init, last))
-    res.send('naada')
-  } catch (err) {
-    console.log(err)
-    res.send(err)
-  }
-})
+//     // res.send(states.features.slice(init, last))
+//     res.send('naada')
+//   } catch (err) {
+//     console.log(err)
+//     res.send(err)
+//   }
+// })
 
+const mayoralties1 = require('../database/mayoralties1.json')
+    const mayoralties2 = require('../database/mayoralties02.json')
 router.route('/mayoralties/:id').get((req, res) => {
   try {
     const { id } = req.params
-    const mayoralties1 = require('../database/mayoralties1.json')
-    const mayoralties2 = require('../database/mayoralties02.json')
     const features = mayoralties1.features.concat(mayoralties2.features)
     const filter = features.filter(data => data.properties.CVE_ENT === id)
     res.send(filter)
