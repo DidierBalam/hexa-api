a4b1f88ce3f015cf57eabff017a1fa9479296fc7
Didier Balam <50980339+DidierBalam@users.noreply.github.com>
2022-07-28 04:18:17 -0500

    return vercel

--- routes/index.js
+++ routes/index.js
@@ -5,26 +5,20 @@ const dbo = require('../db/conn');
 
 router.route('/states/:page').get((req, res) => {
   try {
-    dbo.connectToServer(function (err) {
-      if (err) console.error(err);
-
-      const { page } = req.params
-      console.log('buscando', page)
-      let db_connect = dbo.getDb()
-      const skip = 3 * (parseInt(page) - 1)
-      db_connect
-        .collection("states")
-        .find({})
-        .skip(skip)
-        .limit(3)
-        .toArray(function (err, result) {
-          if (err) throw err;
-          console.log('resolviendo')
-          res.json(result);
-        });
-    })
-
-
+    const { page } = req.params
+    console.log('buscando', page)
+    let db_connect = dbo.getDb()
+    const skip = 3 * (parseInt(page) - 1)
+    db_connect
+      .collection("states")
+      .find({})
+      .skip(skip)
+      .limit(3)
+      .toArray(function (err, result) {
+        if (err) throw err;
+        console.log('resolviendo')
+        res.json(result);
+      });
   } catch (err) {
     console.log(err)
     res.send(err)
---
+++ server.js
@@ -0,0 +1,21 @@
+require('dotenv').config({ path: './config.env' })
+
+const express = require('express')
+const router = require('./routes/index')
+const cors = require('cors')
+const dbo = require('./db/conn')
+
+const PORT = 4000
+const app = express()
+
+app.use(cors())
+app.use(express.json());
+app.use('/api', router)
+
+app.listen(PORT, () => {
+    dbo.connectToServer(function (err) {
+        if (err) console.error(err);
+    });
+    console.log('Servidor corriendo en el puerto', PORT)
+})
+
