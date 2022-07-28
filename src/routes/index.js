99df590075e0c12aa6f3452a34e968ef155ac6d6
Didier Balam <50980339+DidierBalam@users.noreply.github.com>
2022-07-27 21:10:39 -0500

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -22,11 +22,11 @@ const router = express.Router()
 // })
 
 const mayoralties1 = require('../database/mayoralties1.json')
-    const mayoralties2 = require('../database/mayoralties02.json')
+    // const mayoralties2 = require('../database/mayoralties02.json')
 router.route('/mayoralties/:id').get((req, res) => {
   try {
     const { id } = req.params
-    const features = mayoralties1.features.concat(mayoralties2.features)
+    const features = mayoralties1.features
     const filter = features.filter(data => data.properties.CVE_ENT === id)
     res.send(filter)
   } catch (err) {
