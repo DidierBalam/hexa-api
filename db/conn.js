e8e54b37550d53665a143566ddd9e766b1270e12
Didier Balam <50980339+DidierBalam@users.noreply.github.com>
2022-07-28 04:11:49 -0500

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,16 +1,16 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const URI = 'mongodb+srv://didierbalam:didier12345@serverlessinstance0.dobv8.mongodb.net/?retryWrites=true&w=majority';
-
-const client = new MongoClient(URI,
-    {
-        useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1
-    })
-
 
 let dbConnection;
 
 module.exports = {
     connectToServer: function (callback) {
+        const URI = 'mongodb+srv://didierbalam:didier12345@serverlessinstance0.dobv8.mongodb.net/?retryWrites=true&w=majority';
+
+        const client = new MongoClient(URI,
+            {
+                useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1
+            })
+
         client.connect((err, db) => {
             if (err || !db) {
                 return callback(err);
